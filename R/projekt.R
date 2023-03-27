dc30f94d9b56d1814591a277a042856da1d9df04
Natalia Wrobel <nataliawrobel470@gmail.com>
2023-03-27 17:02:08 +0200

    .

--- R/projekt.R
+++ R/projekt.R
@@ -3,4 +3,11 @@ library(ggplot2)
 library(data.table)
 library(reshape2)
 
-filmy = read.csv("movie_data.csv")
+movies = read.csv("movie_data.csv")
+
+titles = data.frame(movies_data$movie_title)
+genres = data.frame(movies_data$genres)
+language = data.frame(movies_data$language)
+content_rating = data.frame(movies_data$content_rating)
+year = data.frame(movies_data$title_year)
+imdb = data.frame(movies_data$imdb_score)
