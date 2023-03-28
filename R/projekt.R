59490626478eb87aa9f39664ebc0f90e2de825db
Natalia Wrobel <nataliawrobel470@gmail.com>
2023-03-28 15:55:45 +0200

    funkcja czas

--- R/projekt.R
+++ R/projekt.R
@@ -12,28 +12,34 @@ movie_data = movie [ , c(1, 2, 3, 5, 6, 7, 8, 10, 11)]
 
 # gatunek
 
-# roku
-rok = function(x){
-  filter(movie_data, movie_data$Released_Year == x)
+# roku od 1920 do 2020
+rok = function(rok){
+  filter(movie_data, movie_data$Released_Year == rok)
 }
 
 # przedził czasu
-przedzial_czasu = function(y,z){
-  filter(movie_data, movie_data$Released_Year >= y & 
-           movie_data$Released_Year <= z)
+przedzial_czasu = function(poczatek_rok, koniec_rok){
+  filter(movie_data, movie_data$Released_Year >= poczatek_rok & 
+           movie_data$Released_Year <= koniec_rok)
 }
 
 # IMDB ocena
-ocena = function(x){
-  filter(movie_data, movie_data$IMDB_Rating == x)
-}
-  
-przedzial_oceny = function(y,z){
-  filter(movie_data, movie_data$Released_Year >= y & 
-           movie_data$Released_Year <= z)
+ocena = function(ocena){
+  filter(movie_data, movie_data$IMDB_Rating == ocena)
 }
 
-# czas trwania filmu kk
-movie_data$Runtime = str_replace_all(movie_data$Runtime, pattern = "[a-zA]+", replacement = "")
+# IMDB przedział ocen od 7.6 do 9.3
+przedzial_oceny = function(najnizsza, najwyzsza){
+  filter(movie_data, movie_data$Released_Year >= najnizsza & 
+           movie_data$Released_Year <= najwyzsza)
+}
 
-movie_data$Runtime = as.integer(movie_data$Runtime) #kfj
+# czas trwania filmu, od 45 do 321
+movie_data$Runtime = str_replace_all(movie_data$Runtime, pattern = "[a-zA]+", 
+                                     replacement = "")
+movie_data$Runtime = as.integer(movie_data$Runtime)
+
+przedzial_czas_trwania = function(x, y){
+  filter(movie_data, movie_data$Runtime >= x &
+           movie_data$Runtime <= y)
+}
