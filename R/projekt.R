7270e4ad600b24d7369b836aeaceeaf340da77a4
Natalia Wrobel <nataliawrobel470@gmail.com>
2023-03-29 23:23:01 +0200

    m

--- R/projekt.R
+++ R/projekt.R
@@ -13,34 +13,34 @@ movies = read.csv("movies.csv")
 
 # roku od 1920 do 2020
 rok_produkcji = function(rok){
-  x = filter(movie_data, movie_data$Released_Year == rok)
+  x = filter(movies, movie == rok)
   print(x, quote = TRUE, row.names = FALSE)
 }
 
 # przedził czasu
 przedzial_czasu = function(poczatek_rok, koniec_rok){
-  filter(movie_data, movie_data$Released_Year >= poczatek_rok & 
+  filter(movies, movie_data$Released_Year >= poczatek_rok & 
            movie_data$Released_Year <= koniec_rok)
 }
 
 # IMDB ocena od 7.6 do 9.3
 ocena_filmu = function(ocena){
-  filter(movie_data, movie_data$IMDB_Rating == ocena)
+  filter(movies, movie_data$IMDB_Rating == ocena)
 }
 
 # IMDB przedział ocen
 przedzial_oceny = function(najnizsza, najwyzsza){
-  filter(movie_data, movie_data$Released_Year >= najnizsza & 
+  filter(movies, movie_data$Released_Year >= najnizsza & 
            movie_data$Released_Year <= najwyzsza)
 }
 
 # czas trwania filmu, od 45 do 321
-movie_data$Runtime = str_replace_all(movie_data$Runtime, pattern = "[a-zA]+", 
+movies$Runtime = str_replace_all(movie_data$Runtime, pattern = "[a-zA]+", 
                                      replacement = "")
-movie_data$Runtime = as.integer(movie_data$Runtime)
+movies$Runtime = as.integer(movie_data$Runtime)
 
 przedzial_czas_trwania = function(x, y){
-  filter(movie_data, movie_data$Runtime >= x &
+  filter(movies, movie_data$Runtime >= x &
            movie_data$Runtime <= y)
 }
 
