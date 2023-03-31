b1529beef22c9519bb00b37e1d1fa328bd6a928a
Natalia Wrobel <nataliawrobel470@gmail.com>
2023-03-31 19:39:54 +0200

    gatunki

--- R/projekt.R
+++ R/projekt.R
@@ -1,47 +1,62 @@
-# pakiety które mogą się przydać
-library(recommenderlab)
+library(shiny)
+library(ggplot2movies)
+library(tidyverse) 
 library(data.table)
-library(reshape2)
 
-# użyte pakiety
-library(stringr)
-library(dplyr)
+movie_data = read.csv("movie_data.csv", stringsAsFactors = FALSE)
 
-movies = read.csv("movie_data.csv")
+movie_data2 = 
+  movie_data %>% 
+  gather(genre, value, drama:western) %>% 
+  filter(value == 1) %>% 
+  select(-value)
 
-# gatunek
+genres =  
+  movie_data2 %>% 
+  distinct(genre) %>% 
+  unlist(.)
 
-# roku od 1920 do 2020
-rok_produkcji = function(rok){
-  x = filter(movies, movie == rok)
-  print(x, quote = TRUE, row.names = FALSE)
+names(genres) = NULL
+
+director = unique(movie_data$director)
+star = unique(movie_data$star)
+
+director_choices = append(director,"All",after = 0)
+star_choices = append(star,"All", after = 0)
+
+ui <- fluidPage(
+  titlePanel("Movie Recommender"), 
+  sidebarLayout( 
+    
+    sidebarPanel( 
+      sliderInput(inputId = "year", label = "Year", min =1920, max = 2020, value = c(1920, 2020),step = 1),
+      sliderInput(inputId = "rating", label = "Rating", min = 7.6, max = 9.3, value = c(7.6, 9.3),step = 0.1),
+      sliderInput(inputId = "runtime", label = "Runtime", min = 45, max = 321, value = c(45, 321),step = 1),
+      selectInput("genres", "Genres", selected = "All", choices = c("All", unique(movie_data2$genre)), multiple = TRUE),
+      
+      selectInput("director_input","Director",director_choices, selected = "All"),
+      selectInput("star_input","Actor",star_choices, selected = "All")
+      ),
+    
+    mainPanel(
+      tableOutput("movie_titles") 
+    )
+  )
+)
+
+server <- function(input, output) { 
+  
+  output$movie_titles = renderTable({
+    m = movie_data2 %>% 
+      filter(between(year, input$year[1],input$year[2])) %>%
+      filter(between(rating, input$rating[1],input$rating[2])) %>%
+      filter(between(runtime, input$runtime[1],input$runtime[2]))
+    
+    if(!("All" %in% input$genres)) {
+      m = m %>% filter(genre %in% input$genres) 
+    }
+    m = m[ ,c(2,9)] 
+  })
 }
 
-# przedził czasu
-przedzial_czasu = function(poczatek_rok, koniec_rok){
-  filter(movies, movie_data$Released_Year >= poczatek_rok & 
-           movie_data$Released_Year <= koniec_rok)
-}
-
-# IMDB ocena od 7.6 do 9.3
-ocena_filmu = function(ocena){
-  filter(movies, movie_data$IMDB_Rating == ocena)
-}
-
-# IMDB przedział ocen
-przedzial_oceny = function(najnizsza, najwyzsza){
-  filter(movies, movie_data$Released_Year >= najnizsza & 
-           movie_data$Released_Year <= najwyzsza)
-}
-
-# czas trwania filmu, od 45 do 321
-
-przedzial_czas_trwania = function(x, y){
-  filter(movies, movie_data$Runtime >= x &
-           movie_data$Runtime <= y)
-}
-
-gatunki = function(z){
-filter(movies, movies$action == z)
-}
-
+shinyApp(ui, server)
