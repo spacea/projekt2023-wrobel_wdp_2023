25a3e6536ecb6ff9b3386321b1aaaf80e044ec76
Natalia Wrobel <nataliawrobel470@gmail.com>
2023-04-01 14:47:35 +0200

    .

--- shiny/movie_shiny.R
+++ shiny/movie_shiny.R
@@ -6,6 +6,8 @@ movie_data = read.csv("movie_data.csv", stringsAsFactors = FALSE)
 director = unique(movie_data$director)
 star = unique(movie_data$star)
 
+director_choices = append(director,"All",after = 0)
+star_choices = append(star,"All", after = 0)
 
 ui <- fluidPage(
   titlePanel("Movie Recommender"), 
@@ -16,7 +18,6 @@ ui <- fluidPage(
       sliderInput(inputId = "rating", label = "Rating", min = 7.6, max = 9.3, value = c(7.6, 9.3),step = 0.1),
       sliderInput(inputId = "runtime", label = "Runtime", min = 45, max = 321, value = c(45, 321),step = 1),
       selectInput("genres", "Genres", selected = "All", choices = c("All", unique(sort(movie_data$genre))), multiple = TRUE),
-      
       selectInput("director_input","Director", director_choices, selected = "All"),
       selectInput("star_input","Actor",star_choices, selected = "All")
     ),
