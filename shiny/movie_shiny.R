9657ed6c294c559ee2f9dee35a9cd66cdd2ea5f3
Natalia Wrobel <nataliawrobel470@gmail.com>
2023-04-04 19:02:58 +0200

    wyszukiwarka

--- shiny/movie_shiny.R
+++ shiny/movie_shiny.R
@@ -20,7 +20,7 @@ random = read.csv("movie_data.csv", stringsAsFactors = FALSE)
 director_choices = append((unique(sort(movie_data$director))),"All", after = 0) 
 star_choices = append((unique(sort(movie_data$star))),"All", after = 0)
 genre_choices = append((unique(sort(movie_data$genre))), "All", after = 0)
-explorer_choices = append((unique(sort(movie_data$title))), "", after = 0)
+title_choices = append((unique(sort(movie_data$title))), "", after = 0)
 
 
 # APLIKACJA- WIDOCZNA DLA UŻYTKOWNIKA
@@ -29,20 +29,22 @@ ui = fluidPage(theme = shinytheme("darkly"),
   navbarPage("Movie Recommender & More",
     tabPanel("Movie Search",
         sidebarLayout(
-        sidebarPanel(
-          sliderInput("year", "Year", min =1920, max = 2020, value = c(1920, 2020),step = 1),
-          sliderInput("rating", "Rating", min = 7.6, max = 9.3, value = c(7.6, 9.3),step = 0.1),
-          sliderInput("runtime", "Runtime", min = 45, max = 321, value = c(45, 321),step = 1),
-          helpText("Select or type for one or more elements. Select 'All' for all elements."),
-          helpText("If you want to select or type for one or more elements, remove 'All' option."),
-          selectInput("genre", "Genres", genre_choices, selected = "All", multiple = TRUE),
-          selectInput("director","Director", director_choices, selected = "All", multiple = TRUE),
-          selectInput("star","Actor",star_choices, selected = "All", multiple = TRUE)
-        ),
-                        
-        mainPanel(
-          tableOutput("movie_titles")
-          ))),
+          sidebarPanel(
+            sliderInput("year", "Year", min =1920, max = 2020, value = c(1920, 2020),step = 1),
+            sliderInput("rating", "Rating", min = 7.6, max = 9.3, value = c(7.6, 9.3),step = 0.1),
+            sliderInput("runtime", "Runtime", min = 45, max = 321, value = c(45, 321),step = 1),
+            helpText("Select or type for one or more elements. Select 'All' for all elements."),
+            helpText("If you want to select or type for one or more elements, remove 'All' option."),
+            selectInput("genre", "Genres", genre_choices, selected = "All", multiple = TRUE),
+            selectInput("director","Director", director_choices, selected = "All", multiple = TRUE),
+            selectInput("star","Actor",star_choices, selected = "All", multiple = TRUE)
+            ),
+          
+          mainPanel(
+            tableOutput("movie_titles")
+          )
+      )
+  ),
              
     tabPanel("Random Movie", 
         sidebarPanel(
@@ -52,15 +54,20 @@ ui = fluidPage(theme = shinytheme("darkly"),
                       
         mainPanel(
           tableOutput("movie_random")
-        )),
+        )
+        ),
              
     tabPanel("Movie Information",
         sidebarPanel(
-        helpText("Select or type to get information about a movie"),
-        selectInput("explorer", "Movie Title", explorer_choices, multiple = FALSE)
+          helpText("Select or type to get information about a movie"),
+          selectInput("title", "Movie Title", title_choices, multiple = FALSE)
         ),
-  )
-  )
+        
+        mainPanel(
+          uiOutput("movie_title")
+        )
+        ),
+    )
 )
 
 
@@ -98,8 +105,22 @@ server <- function(input, output, session) { # funkcja zakładająca dane wejśc
       select(title, director, star, genre, year, rating, runtime) 
     output$movie_random = renderTable(x)
   })
-  
-  
+
+  output$movie_title = renderUI({
+    y = movie_data 
+    if(!("" %in% input$title)) { 
+      y = y %>% filter(title %in% input$title)
+      HTML(paste0( h3(y$title), br(), 
+                   h4("Overview"), y$overview, br(), br(), 
+                   h4("Genres"), y$genre, br(), br(), 
+                   h4("Year"), y$year, br(), br(),
+                   h4("Director"), y$director, br(), br(),
+                   h4("Actor"), y$star, br(), br(),
+                   h4("Runtime"), y$runtime, " min", br(), br(),
+                   h4("Rating"), y$rating))
+      }
+    })  
+ 
 }
 
 # załączenie aplikacji
